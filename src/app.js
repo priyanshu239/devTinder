b6ac791222fd59ecb1d98592f49fc08f8053c7a9
priyanshu239 <priyanshuguptaablack@gmail.com>
2026-01-24 11:19:40 +0530

    end

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 
 // Reads the json object convert it into javascript object
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -56,7 +56,7 @@ authRouter.post("/login", async(req,res) => {
             res.cookie("token",token, {
                 expires: new Date(Date.now()+ 8*3600000)} 
             );
-            res.send("login succesful");
+            res.send(user);
 
         } else {
             throw new Error("Incorrect Password");
